db59f229f3d8e8fc4d54d0ac01c8458ec540fa49
Gavin Kennedy <gavin@albanylan.com>
2021-09-29 14:16:56 +1000

    Initial commit

---
+++ BlueIrisCleanup.py
@@ -0,0 +1,214 @@
+import winreg
+import os
+from winreg import *
+from os import listdir
+from os.path import isfile, join
+from os import walk
+import glob
+import ctypes
+import platform
+import sys
+import wmi
+from datetime import datetime, time, timedelta
+import shutil
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
+
+yesterday_midnight = (datetime.combine(datetime.today(), time.min)) - timedelta(days=1)
+
+NewClipsKey = r"SOFTWARE\Perspective Software\Blue Iris\clips\folders\0"
+StoredClipsKey = r"SOFTWARE\Perspective Software\Blue Iris\clips\folders\1"
+AlertsClipsKey = r"SOFTWARE\Perspective Software\Blue Iris\clips\folders\2"
+
+newDict = {}
+storedDict = {}
+alertsDict = {}
+
+actions_taken = []
+
+def send_email(cctv_server, actions_taken):
+
+    me = "blueiris-script@lindisfarne.nsw.edu.au"
+    you = "gkennedy@lindisfarne.nsw.edu.au"
+    bcc = ""
+
+    you_list = you.split(",")
+    bcc_list = bcc.split(",")
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Blue Iris Cleanup Script - " + cctv_server
+    msg['From'] = formataddr((str(Header('Blue Iris Cleanup Script', 'utf-8')), me))
+    msg['To'] = you
+
+    email_text = "<p><span style=\"font-weight: 400;\">The following actions were taken:<br><br>"
+    
+    for action in actions_taken: 
+        email_text = email_text + action + "<br>"
+        
+    email_text = email_text + "</span></p>"
+
+    email_body = MIMEText(email_text, 'html')
+
+    msg.attach(email_body)
+
+    s = smtplib.SMTP('smtp.lindisfarne.nsw.edu.au', 25)
+
+    email_result = s.sendmail(me, you_list + bcc_list, msg.as_string())
+
+    s.quit()
+
+def getFreeSpace(drive_letter):
+    c = wmi.WMI ()
+    retval = 0
+    for d in c.Win32_LogicalDisk():
+        if (d.Caption.upper() == drive_letter.upper()):
+            retval = d.FreeSpace
+    return retval
+    
+
+# Get the New information
+key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, NewClipsKey, 0, winreg.KEY_READ)
+try:
+    count = 0
+    while 1:
+        name, value, type = winreg.EnumValue(key, count)
+        newDict[name] = value  
+        count = count + 1
+except WindowsError:
+    pass
+    
+# Get the Stored information
+key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, StoredClipsKey, 0, winreg.KEY_READ)
+try:
+    count = 0
+    while 1:
+        name, value, type = winreg.EnumValue(key, count)
+        storedDict[name] = value  
+        count = count + 1
+except WindowsError:
+    pass
+
+# Get the Alerts information
+key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, AlertsClipsKey, 0, winreg.KEY_READ)
+try:
+    count = 0
+    while 1:
+        name, value, type = winreg.EnumValue(key, count)
+        alertsDict[name] = value  
+        count = count + 1
+except WindowsError:
+    pass    
+
+# Enumerate files inside each folder
+newFiles = glob.glob(newDict["path"] + "\*")
+storedFiles = glob.glob(storedDict["path"] + "\*")
+alertsFiles = glob.glob(alertsDict["path"] + "\*")
+
+# Get the drive letters for each folder
+newFolderDriveLetter = newDict["path"].split(":")[0]
+storedFolderDriveLetter = storedDict["path"].split(":")[0]
+alertsFolderDriveLetter = alertsDict["path"].split(":")[0]
+
+# Get the free space on each folder's drive in Gigabytes
+newFolderFreeSpace = int((str(getFreeSpace(newFolderDriveLetter + ":")))) / 1024 / 1024 / 1024
+storedFolderFreeSpace = int((str(getFreeSpace(storedFolderDriveLetter + ":")))) / 1024 / 1024 / 1024
+alertsFolderFreeSpace = int((str(getFreeSpace(alertsFolderDriveLetter + ":")))) / 1024 / 1024 / 1024
+
+# New folder
+# If free space is less than 5GB, do something
+if (newFolderFreeSpace < 5):
+    print("New folder free space is less than 5GB, seeing what we can do...")
+    # Move the oldest file to the Stored folder
+    for file in newFiles:
+        st=os.stat(file)    
+        mtime=st.st_mtime
+        if (float(mtime) < float(yesterday_midnight.timestamp())):
+            print("File '" + file + "' is older than today, moving to Stored folder")
+            print("Moving '" + file + "' to '" + storedDict["path"])
+            try:
+                shutil.move(file, storedDict["path"] + "\\")
+                print(" - Successfully moved the file")
+                actions_taken.append("Moved New file " + file + " to the Stored folder")
+            except:
+                print("There was an error moving file '" + file + "'")
+                actions_taken.append("Error moving New file " + file + " to the Stored folder")
+        else:
+            #print("File '" + file + "' has today's timestamp, ignoring")
+            pass
+else:
+    print("New folder free space is greater than 5GB, don't need to do anything")
+    
+# Stored folder
+# If free space is less than 5GB, do something
+if (len(actions_taken) == 0):
+    if (newFolderFreeSpace < 5):
+        print("New folder free space is less than 5GB, seeing what we can do...")
+        # Delete the oldest file
+        oldest_file = ""
+        oldest_file_age = 0
+        
+        for file in storedFiles:
+            st=os.stat(file)    
+            mtime=st.st_mtime
+            if (float(mtime) < float(yesterday_midnight.timestamp())):
+                if (float(mtime) < oldest_file_age or oldest_file_age == 0):
+                    oldest_file = file
+                    oldest_file_age = mtime
+            else:
+                pass
+
+        #print(oldest_file)
+        #print(oldest_file_age)
+
+        if (oldest_file != ""):
+            print("File '" + oldest_file + "' is older than today, and is the oldest file in the Stored folder")
+            print("Deleting '" + oldest_file + "'")
+            try:
+                os.remove(oldest_file)
+                print(" - Successfully deleted the file")
+                actions_taken.append("Deleted Stored file " + file)
+            except:
+                print("There was an error deleting the file '" + file + "'")
+                actions_taken.append("Error deleting Stored file " + file)
+
+    else:
+        print("Stored folder free space is greater than 5GB, don't need to do anything")
+
+# Alerts folder
+# If free space is less than 5GB, do something
+if (len(actions_taken) == 0):
+    if (alertsFolderFreeSpace < 40):
+            print("Alerts folder free space is less than 5GB, seeing what we can do...")
+            # Delete the oldest file
+            oldest_file = ""
+            oldest_file_age = 0
+            
+            for file in alertsFiles:
+                st=os.stat(file)    
+                mtime=st.st_mtime
+                if (float(mtime) < float(yesterday_midnight.timestamp())):
+                    if (float(mtime) < oldest_file_age or oldest_file_age == 0):
+                        oldest_file = file
+                        oldest_file_age = mtime
+                else:
+                    pass
+
+            #print(oldest_file)
+            #print(oldest_file_age)
+
+            if (oldest_file != ""):
+                print("File '" + oldest_file + "' is older than today, and is the oldest file in the Alerts folder")
+                print("Deleting '" + oldest_file + "'")
+                try:
+                    os.remove(oldest_file)
+                    print(" - Successfully deleted the file")
+                    actions_taken.append("Deleted Alert file " + file)
+                except:
+                    print("There was an error deleting the file '" + file + "'")
+                    actions_taken.append("Error deleting Alert file " + file)
+
+if (len(actions_taken) > 0):
+    send_email("Gavin's Server", actions_taken)
